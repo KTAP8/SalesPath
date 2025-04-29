506cab746f48d037bc96ee79c65ae0d1815e1375
KTAP8 <touch.apinankul@gmail.com>
2025-04-29 14:35:40 +0700

    feat: update the new (screens) layout to be Tabs instead of Stack

--- SalesPathNative/app/(screens)/_layout.tsx
+++ SalesPathNative/app/(screens)/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
+//we use tabs instead of stack so that the sidebar and pages doesn't gets re-render everytime
 export default function ScreensLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: { display: "none" }, // Hides the bottom tab bar
+      }}
+    />
+  );
 }
